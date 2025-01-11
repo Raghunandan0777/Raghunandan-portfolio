933ac6142b7881ca2071249ece24441bcc3b1b3a
malay77patra <malay77patra@gmail.com>
2025-01-11 12:56:20 +0530

    removed orbiting

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,12 @@ import Tooltip from './components/Tooltip'
 import './styles/app.css'
 import Home from './sections/Home'
 import Cursor from './components/Cusror'
-import Orbiting from './components/animations/Orbiting'
 
 function App() {
 
 
   return (
     <>
-    <Orbiting>
-      <h1>Orbiting</h1>
-    </Orbiting>
       <Home />
       <Dock />
       <Tooltip />
