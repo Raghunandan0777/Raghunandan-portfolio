332ac8b78d3feac8e76c348ae6b69014bca05d34
malay77patra <malay77patra@gmail.com>
2025-04-30 20:18:56 +0530

    update resume link

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -12,7 +12,7 @@ export default function About() {
         I’m a <a href="#">First year student</a>, having a passion towards building dynamic and efficient web applications using the <a href="#">MERN stack</a>. Over the past two years, I’ve worked as a <a href='#'>freelancer</a>, including a notable stint with <a href='#'>Vino AI</a>, honing my expertise in web development and automation. I also integrate <a href='#'>AI and APIs</a> into prebuilt systems to enhance functionality. Actively seeking for new opportunities as a part time, flexible full time Full stack engineer.
       </p>
       <p>Let’s build something great! ✨</p>
-      <a href="https://drive.google.com/file/d/1KIIvBZnJ1M9YltAVd6pnbtVzHrXXa5u2/view" target='_blank'>
+      <a href="https://drive.google.com/file/d/1ndeSxjR6InKTRdrs69jDwf1PTqHN_JSt/view" target='_blank'>
         <Button1>
           <SvgIcon icon={Pdf} />
           Resume
