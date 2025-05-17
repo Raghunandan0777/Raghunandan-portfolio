8fd0b9acfb5e163d0fffcec197d2560228b9cbbe
malay77patra <malay77patra@gmail.com>
2025-05-17 18:38:13 +0530

    progress: added analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from './context/ThemeContext.jsx'
 import './styles/index.css'
 import App from './App.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <ThemeProvider>
     <App />
+    <Analytics />
   </ThemeProvider>,
 )
